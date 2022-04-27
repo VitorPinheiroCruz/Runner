f2cdaa5970d290d09deb2ed09b11cfcb00ebc13c
VitorPinheiroCruz <vitorcafe2011@gmail.com>
2022-04-27 08:55:12 -0300

    atualizando o código

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from sys import exit
-
 # import random
 
 pygame.init()
@@ -13,7 +12,8 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-score_surf = test_font.render('My game', False, 'black')  # .render(text, AA, color)
+
+score_surf = test_font.render('My game', False, (64,64,64))  # .render(text, AA, color)
 score_rect = score_surf.get_rect(center=(400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -45,6 +45,10 @@ while True:
 
     screen.blit(ground_surface, (0, 300))
     screen.blit(sky_surface, (0, 0))  # .blit(surface, position)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    # pygame.draw.line(screen, 'blue', (0,0), pygame.mouse.get_pos(), 3) #desenhando uma linha
+    # pygame.draw.ellipse(screen, 'gold', pygame.Rect(50,200,100,100))  # desenhando uma elípse
     screen.blit(score_surf, score_rect)
     # snail_x_pos -= 4
     # if snail_x_pos < -50:
@@ -66,3 +70,5 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
+
+    01:29:37
