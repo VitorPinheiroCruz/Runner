af34ce81b3f9fbd1c4c0801159814c946b689adb
VitorPinheiroCruz <vitorcafe2011@gmail.com>
2022-04-27 19:21:43 -0300

    atualizado até 02:38:23

--- comands_pygame.py
+++ comands_pygame.py
@@ -19,7 +19,7 @@ def obstacle_movement(obstacle_list):
 
         return obstacle_list
     else:
-        return []
+        return  []
 
 def game_message():
     text_font = pygame.font.Font('font/Pixeltype.ttf', 30)
@@ -46,21 +46,35 @@ score = 0
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
+# score_surf = test_font.render('My game', False, (64,64,64))  # .render(text, AA, color)
+# score_rect = score_surf.get_rect(center=(400,50))
+
 # Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(600, 300))
+# snail_x_pos = 600
+# snail_y_pos = 270
+
 obstacle_rect_list = []
 
-# Player
+
+
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 player_gravity = 0
+# test_surface = pygame.Surface((100,200))
+# test_surface.fill('Red')
 
 # Intro screen
 player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+# # player_stand = pygame.transform.scale2x(player_stand)
 player_stand_rect = player_stand.get_rect(center=(400,200))
 
+# player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+# player_stand_scaled = pygame.transform.scale(player_stand, (200, 400))
+# player_stand_rect = player_stand_scaled.get_rect(center=(400,200))
+# # player_stand_rect = player_stand.get_rect(midbottom=(400,200))
 
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
@@ -72,7 +86,8 @@ while True:
             print('Vc clicou no x')
             pygame.quit()
             exit()
-
+        # solução do desenvolver para recomeçar o jogo
+        # habilidade de pular quando o mouse clica no player - my design
         if game_active:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos):
@@ -91,31 +106,103 @@ while True:
 
         if event.type == obstacle_timer and game_active:
             obstacle_rect_list.append(snail_surf.get_rect(bottomright=(randint(900, 1100), 300)))
-            print('timer')
+            # print('test')
+
+        # # habilidade de pular quando o mouse clica no player - my design
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     if player_rect.collidepoint(event.pos):
+        #         if player_rect.bottom >= 300:
+        #             player_gravity = -20
+        #
+        # # habilidade de pular pressionando o space
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+        #         player_gravity = -20
+        #
+        # # após game over, recomeçar o jogo pressionando o espaço
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE: game_active = True
+
+        # print jump toda vez que a tecla space é pressionada
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE:
+        #         print('jump')
+
+        # print key-down toda vez que uma tecla é pressionada
+        # print key-up toda vez que uma tecla é pressionada
+        # if event.type == pygame.KEYDOWN:
+        #     print('key-down')
+        # if event.type == pygame.KEYUP:
+        #     print('key-up')
+
+        # if event.type == pygame.MOUSEMOTION:  # ler e imprime a posição do mouse
+        #     print(event.pos)
+
+        # ler e imprime se houve clique
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     print('mouse down')
+
+        # colisão pelo evento
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('collision')
 
     if game_active:
         screen.blit(ground_surface, (0, 300))
         screen.blit(sky_surface, (0, 0))  # .blit(surface, position)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # screen.blit(score_surf, score_rect)
         score = display_score()
+        # pygame.draw.line(screen, 'blue', (0,0), pygame.mouse.get_pos(), 3) #desenhando uma linha
+        # pygame.draw.ellipse(screen, 'gold', pygame.Rect(50,200,100,100))  # desenhando uma elípse
+        # snail_x_pos -= 4
+        # if snail_x_pos < -50:
+        #     snail_x_pos = 800
+        #     snail_y_pos = random.randint(270, 360)
+        # snail_rect.x -= 4
+        # if snail_rect.right < 0: snail_rect.left = 800
+        # screen.blit(snail_surf, snail_rect)
 
         # Player gravidade
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300: player_rect.bottom = 300
+        # colocando gravidade do player
         screen.blit(player_surf, player_rect)
+        # screen.blit(player_surf, (player_rect[0], player_rect[1] + player_gravity))
+        # screen.blit(player_surf, player_rect)
 
         # Obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+
+
+
+        # if player_rect.colliderect(snail_rect):
+        #     print('collision')
+        # print jump toda vez que a tecla spaço é pressionada
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_SPACE]:
+        #     print('jump')
+
+        # mouse_pos = pygame.mouse.get_pos()
+        # if player_rect.collidepoint(mouse_pos):   # .collidepoint((x,y))
+        #     print('collision')
+        # if player_rect.collidepoint(mouse_pos):
+        #     print(pygame.mouse.get_pressed())  # leitura do clique do mouse
 
         # Collision
         if player_rect.colliderect(snail_rect):
             print('houve uma colisão com o snail')
             game_active = False
+            # pygame.quit()
+            # exit()
     else:
         snail_rect.left = 800
         start_time = pygame.time.get_ticks()
         screen.fill((94,129,162))
         name_game()
+        # screen.fill('yellow')
         screen.blit(player_stand, player_stand_rect)
         score_message = test_font.render(f'You score: {score/1000:.2f}',False, (111,196,169))
         score_message_rect = score_message.get_rect(center=(400,330))
